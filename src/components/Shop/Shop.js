e57bdffb737f264b4aa4629170ac2d38a4b66db4
mahmudshumit <rahmanshumit@gmail.com>
2021-02-27 00:06:29 +0600

    commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css'
 
 const Shop = () => {
-    const first10 =fakeData.slice(0,10);
-   
-    const [products,setProducts] =useState(first10);
-   
+    const first10 = fakeData.slice(0, 10);
+
+    const [products, setProducts] = useState(first10);
+
     return (
-        <div>
-           <h1>This Is Shop</h1>
-           <h3>{products.length}</h3>
-           <ul>
-               {
-                   products.map(product=><li>{product.name}</li>)
-               }
-           </ul>
+        <div className="shop-container">
+            <div className="product-container">
+
+                {
+                    products.map(product => <Product product={product}></Product>)
+                }
+
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
+
+
+
         </div>
     );
 };
